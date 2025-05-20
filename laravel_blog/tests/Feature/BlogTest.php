de776c6f9fe8b84f5d3f5e114d3119a85b238174
DukVyacheslav <duvn@mail.ru>
2025-05-20 13:48:32 +0300

    Исправить тесты блога: обновить названия полей на 'name' и 'text', изменить маршрут на '/blog' для создания поста.

--- laravel_blog/tests/Feature/BlogTest.php
+++ laravel_blog/tests/Feature/BlogTest.php
@@ -14,16 +14,35 @@ class BlogTest extends TestCase
     public function it_creates_a_blog_post()
     {
         $data = [
-            'title' => 'Тестовый пост',
-            'content' => 'Контент тестового поста',
+            'name' => 'Тестовый пост',
+            'text' => 'Контент тестового поста',
         ];
 
-        $response = $this->post('/admin/blog/upload', $data);
+        $response = $this->post('/blog', $data);
 
         $response->assertStatus(302); // редирект после успешного создания
         $this->assertDatabaseHas('blogs', [
-            'title' => $data['title'],
-            'content' => $data['content'],
+            'name' => $data['name'],
+            'text' => $data['text'],
         ]);
+    }
+
+    /** @test */
+    public function it_deletes_a_blog_post()
+    {
+        $post = Blog::factory()->create();
+
+        $response = $this->delete("/blog/{$post->id}");
+
+        $response->assertStatus(302); // редирект после успешного удаления
+        $this->assertDatabaseMissing('blogs', ['id' => $post->id]);
+    }
+
+    /** @test */
+    public function it_validates_blog_post_creation()
+    {
+        $response = $this->post('/blog', []);
+
+        $response->assertSessionHasErrors(['name', 'text']);
     }
 }
